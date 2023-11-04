12211e5731c8711f657b383644dde850c3e9bd24
pdxboyan <pdx.boyan@gmail.com>
2023-11-04 15:11:42 -0700

    hopefully this will refresh the page

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+// refreshes the page once after load to prevent buggy load
+
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
+
 let toggle = document.querySelector("#header .toggle-button");
 let collapse = document.querySelectorAll("#header .collapse");
 
