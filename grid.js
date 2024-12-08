f8dda81d9e410eef8fa48e95109ca2ac0b48f44d
boga2-psu <boga2@pdx.edu>
2024-12-08 01:33:27 -0800

    Add: grid.js to use Masonry & imagesLoaded library for neat picture grid on landing page ... & landing page complete

---
+++ grid.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    var grid = document.querySelector('.grid');
+
+    // Creates a Masonry grid selecting .grid-items as the tiles
+    var masonry = new Masonry(".grid", {
+        itemSelector: '.grid-item',
+        gutter: 20
+    });
+
+    imagesLoaded(grid).on('progress', function() {
+        masonry.layout();
+    });
+};
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // recalculte Masonry grid after each image loads
+  masonry.layout();
+});
