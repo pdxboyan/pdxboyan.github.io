d7d10cf977c2bc09ea7f3a5a6257e86bc45add4b
pdxboyan <pdx.boyan@gmail.com>
2024-12-24 12:14:35 -0800

    Reverted old load to be more intensive - it unfortunetly looks cleaner

--- grid.js
+++ grid.js
@@ -1,19 +1,18 @@
 window.onload = function () {
     var grid = document.querySelector('.grid');
 
-    // Create a Masonry grid selecting .grid-items as the tiles
+    // Creates a Masonry grid selecting .grid-items as the tiles
     var masonry = new Masonry(".grid", {
         itemSelector: '.grid-item',
         gutter: 20
     });
 
-    // Get the last image by its ID
-    var lastImage = document.getElementById('last-image');
-
-    // Trigger Masonry layout only after the last image loads
-    if (lastImage) {
-        lastImage.onload = function () {
-            masonry.layout();
-        };
-    }
+    imagesLoaded(grid).on('progress', function() {
+        masonry.layout();
+    });
 };
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // recalculte Masonry grid after each image loads
+  masonry.layout();
+});
